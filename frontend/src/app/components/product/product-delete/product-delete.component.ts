e61debc8c9db3bbdf9110e5069d091d75739e076
Rafael Veloso <rafaelveloso11@gmail.com>
2021-11-29 19:43:15 -0300

    Carga inicialt

---
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,35 @@
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+  product: Product
+
+  constructor(private ProductService : ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.ProductService.readById(id).subscribe(product => {
+      this.product = product
+    })
+  }
+
+  deleteProduct():void{
+      this.ProductService.delete(this.product.id).subscribe(()=>{
+        this.ProductService.showMessage('Produto excluido com sucesso')
+        this.router.navigate(['/products'])      
+      })
+  }
+
+  cancel():void{
+    this.router.navigate(['/products'])
+  }
+
+}
